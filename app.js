b15bcf76fb742062cb051a126727d0caaaf7e0f6
Akshay <akshaywake11@gmail.com>
2025-01-11 17:16:36 +0530

    easy game

---
+++ app.js
@@ -0,0 +1,63 @@
+let gameSeq = [];
+let userSeq = [];
+
+let btn = ["yellow","red", "purple" , "green"]
+
+let started = false;
+let level = 0;
+
+let h2 = document.querySelector("h2");
+
+document.addEventListener("keypress",function(){
+  if(started==false){
+     console.log("game is started");
+     started = true;
+
+     levelUp();
+  }
+});
+
+function gameFlash(btn){
+   btn.classList.add("flash");
+   setTimeout(function (){
+    btn.classList.remove("flash");
+   },250);
+}
+
+function userFlash(btn){
+  btn.classList.add("userflash");
+  setTimeout(function (){
+   btn.classList.remove("userflash");
+  },250);
+}
+
+
+function levelUp(){
+   level++;
+   h2.innerText = `Level ${level}`;
+
+   let randIdx = Math.floor(Math.random()*3);
+   let randColor = btns[randIdx];
+   let randBtn = document.querySelector(`.${randColor}`)
+   gameSeq.push(randColor);
+   console.log(gameSeq);
+   gameFlash(randBtn);
+}
+
+function btnPress(){
+   let btn = this;
+   gameFlash(btn);
+   userFlash(btn);
+  
+   userColor = btn.getAttribute("id");
+   console.log(userColor);
+
+}
+
+let allBtns = document.querySelectorAll(".btn");
+for(btn of allBtns){
+   btn.addEventListener("click", btnPress);
+}
+
+
+  
